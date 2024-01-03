be3c191d30c7f5e53a569fedf255c0e07116433d
sangyeol9 <xhdfud1@naver.com>
2024-01-03 12:30:14 +0900

    20240103main(1)

--- src/main/java/com/winter/app/regions/RegionController.java
+++ src/main/java/com/winter/app/regions/RegionController.java
@@ -5,8 +5,10 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping(value="/regions/*")
@@ -24,20 +26,22 @@ public class RegionController  {
 		return "regions/add";
 	}
 	@RequestMapping(value="add", method = RequestMethod.POST)
-	public String add(HttpServletRequest request) throws Exception {
-		String id =  request.getParameter("region_id");
-		String name = request.getParameter("region_name");
-		RegionDTO dto = new RegionDTO();
-		dto.setRegion_id((Integer.valueOf(id)));
-		dto.setRegion_name(name);
+	public String add(RegionDTO dto,Model model) throws Exception {
+//		String id =  request.getParameter("region_id");
+//		String name = request.getParameter("region_name");
+//		RegionDTO dto = new RegionDTO();
+//		dto.setRegion_id((Integer.valueOf(id)));
+//		dto.setRegion_name(name);
 		int result = this.dao.addRegion(dto);
 		
 		String msg ="등록 실패";
 		if(result>0) {
 			msg = "등록 성공";
 		}
-		request.setAttribute("msg", msg);
-		request.setAttribute("path", "./list");
+//		request.setAttribute("msg", msg);
+//		request.setAttribute("path", "./list");
+		model.addAttribute("msg", msg);
+		model.addAttribute("path", "./list");
 		return "commons/result";
 	}
 	
@@ -53,14 +57,13 @@ public class RegionController  {
 		return "regions/list";
 	}
 	@RequestMapping(value="detail", method = RequestMethod.GET)
-	public String detail(HttpServletRequest request) throws Exception{
+	public String detail(Integer region_id,Model model) throws Exception{
 		
 		RegionDTO dto = new RegionDTO();
-		String id = (request.getParameter("region_id"));
-		dto.setRegion_id(Integer.valueOf(id));
+		dto.setRegion_id(region_id);
 		dto = dao.getDetail(dto);
-		request.setAttribute("detail", dto);
-		
+		//request.setAttribute("detail", dto);
+		model.addAttribute("detail", dto);
 		
 		return "regions/detail";
 	}
